09fb8538f9aafab985e827f50ff74b91024ec748
Raz <razefron01@gmail.com>
2020-07-08 13:36:28 +0300

    FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -10,7 +10,7 @@ export default function App() {
   }
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+    setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: enteredGoal }])
   }
 
   return (
@@ -24,15 +24,12 @@ export default function App() {
           />
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
-      <ScrollView>
-        {courseGoals.map((goal, index) => 
-          <View 
-            key={index} 
-            style={styles.listItem}>
-              <Text>{goal}</Text>
-          </View>
-            )}
-      </ScrollView>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => (<View
+          style={styles.listItem}>
+          <Text>{itemData.item.value}</Text>
+        </View>)}/>
     </View>
   );
 }
